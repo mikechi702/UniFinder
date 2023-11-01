3818b355420f71a44210e09127d15a9c95147555
mikechi702 <mikechicarroll702@gmail.com>
2023-11-01 13:21:24 -0500

    Routing chatgpt to frontend

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask_cors import CORS
 from flask import Flask, jsonify
-# import openAItest
+from openAItest import setModelPrompt, setMessage, getResponse
 
-app = Flask(__name__)
+app = Flask(__name__) # initial app setup
 CORS(app)
 
-@app.route('/')
+@app.route('/') # base route
 def hello():
     return "hello in flask"
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/api/data', methods=['GET']) # api setup to send data to the frontend
 def get_data():
-    data = {'message': 'Hello from Flask!'}
+    setModelPrompt("You are a helpful assistant")
+    setMessage("Give me a sleazy lawyer slogan")
+    aiMessage = "placeholder"
+    aiMessage = getResponse()
+    data = {'message': aiMessage}
     return jsonify(data)
 
 if __name__ == '__main__':
--- openAItest.py
+++ openAItest.py
@@ -8,16 +8,35 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 if openai.api_key is None:
     raise ValueError("OpenAI API key is not set in the environment variables.")
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Give me a sleazy lawyer slogan"},
-    ],
-    max_tokens = 50,
-)
-print(response['choices'][0]['message']['content'])
+modelPrompt = "You are a helpful assistant"
+message = "Give me a sleazy lawyer slogan"
 
-# print("test123")
+def setModelPrompt(inputPrompt):
+    global modelPrompt
+    modelPrompt = inputPrompt
+
+def setMessage(inputMessage):
+    global message
+    message = inputMessage
+
+setModelPrompt("You are a helpful assistant, but you also give your responses with a snarky and sarcastic ring")
+setMessage("In one sentence, give me an apology from a fossil fuel company that ruined an ecosystem")
+
+def getResponse():
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": modelPrompt},
+            {"role": "user", "content": message},
+        ],
+        max_tokens = 50,
+    )
+
+    return response['choices'][0]['message']['content']
+
+if __name__ == '__main__': # bandaid fix to prevent these functions running automatixally
+    getResponse()
+    setModelPrompt()
+    setMessage()
 
 
