b881e219f581ecb4c82628aa26154d41fe8b1e90
mikechi702 <mikechicarroll702@gmail.com>
2023-11-17 14:26:59 -0600

    Chat routed between frontend and backend w user input

--- unifinder/src/App.js
+++ unifinder/src/App.js
@@ -4,28 +4,57 @@ import axios from 'axios';
 
 function App() {
   const [data, setData] = useState({});
+  const [userInput, setUserInput] = useState('');
 
   useEffect(() => {
-    // Use Axios to make the API request
-    axios.get('http://localhost:5000/api/data')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    fetchData(); // Initial data fetch
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/data');
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/data', { name: userInput });
+
+      if (response.status === 200) {
+        setData(response.data);
+        setUserInput('');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="App">
       <h1>React Frontend</h1>
       <p>{data.message}</p>
+      
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your input:
+          <input
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
 
 export default App;
-
 
 
 
